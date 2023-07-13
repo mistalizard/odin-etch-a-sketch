cb55aa48e0bff0ff6781b4890d9d826e642dc793
Tim Miller <xxtimtimxx@gmail.com>
2023-07-13 02:33:47 -0400

    Added hover effect to canvas
    On canvas creation, each div gets an event listener that toggles the
    'on' class. Added CSS to change background color for the 'on' class.

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const grid = document.querySelector('#grid-container')
 
-// for (let i = 1; i <= 16; i++) {
-//   const row = document.createElement('div')
-//   row.classList = 'grid-row'
-//   row.id = `row${i}`
-//   for (let i = 1; i <= 16; i++) {
-//     const square = document.createElement('div')
-//     square.textContent = i
-//     square.classList = 'grid-square'
-//     row.appendChild(square)
-//   }
-//   grid.appendChild(row)
-// }
-
 for (let i = 1; i <= 16; i++) {
   for (let i = 1; i <= 16; i++) {
     const square = document.createElement('div')
     square.textContent = i
     square.classList = 'grid-square'
+    square.addEventListener('mouseover', () => {
+      square.classList.toggle('on')
+    })
     grid.appendChild(square)
   }
 }
