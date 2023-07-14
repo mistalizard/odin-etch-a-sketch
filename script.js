1f6c979c7ee529b3689947052df49ff67361df7e
Tim Miller <xxtimtimxx@gmail.com>
2023-07-14 01:25:28 -0400

    Added custom canvas sizing
    Set up 3 container divs in a flex layout. Grid defaults to 16x16 on
    page load. Added button to allow custom grid sizing. All grid squares
    are dynamically sized in the JS to ensure they fit container evenly.

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const grid = document.querySelector('#grid-container')
+const canvasSizeButton = document.querySelector('#canvas-size')
+let canvasSize = 96
 
-for (let i = 1; i <= 16; i++) {
-  for (let i = 1; i <= 16; i++) {
-    const square = document.createElement('div')
-    square.textContent = i
-    square.classList = 'grid-square'
-    square.addEventListener('mouseover', () => {
-      square.classList.toggle('on')
-    })
-    grid.appendChild(square)
+function clearGrid() {
+  const elements = document.querySelectorAll('.grid-square')
+  elements.forEach(element => {
+    element.remove()
+  })
+}
+
+function resizeGrid() {
+  let gridSize = 0
+  while (gridSize < 4 || gridSize > 100) {
+    gridSize = prompt('Enter a number between 4-100 to change canvas size')
+    if (gridSize < 4 || gridSize > 100) {
+      alert('Invalid Size: Size must be between 4-100')
+    }
+  }
+  console.log(gridSize)
+  clearGrid()
+  createGrid(gridSize)
+}
+
+function createGrid(size) {
+  for (let i = 1; i <= size; i++) {
+    for (let i = 1; i <= size; i++) {
+      const square = document.createElement('div')
+      //   square.textContent = i
+      square.classList = 'grid-square'
+      square.style.width = `${100 / size}%`
+      square.style.height = `${100 / size}%`
+      square.addEventListener('mouseover', () => {
+        square.classList.toggle('on')
+      })
+      grid.appendChild(square)
+    }
   }
 }
+
+canvasSizeButton.addEventListener('click', () => {
+  resizeGrid()
+})
+
+createGrid(canvasSize)
